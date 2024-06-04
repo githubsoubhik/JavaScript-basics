8b35f3e8cc116bf93975a440af78d36522a64c85
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-06-04 12:43:32 +0000

    commit

---
+++ 1st/datatype.js
@@ -0,0 +1,23 @@
+"use strict"  //javacript treat as a newer version
+
+//tc39.es is the original documentation
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
